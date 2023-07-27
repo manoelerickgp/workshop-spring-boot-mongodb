eb30640f6c95fcf04426906523b314448fbc4707
manoelerickgp <manoel.computacao@gmail.com>
2023-07-27 18:42:28 -0300

    Primerio commit starter test

---
+++ src/test/java/com/myprojeto/workshopMongoDB/WorkshopMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojeto.workshopMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
