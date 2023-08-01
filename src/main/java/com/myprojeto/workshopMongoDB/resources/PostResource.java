1880701d09b6074e891e4140573375cc970e71df
manoelerickgp <manoel.computacao@gmail.com>
2023-08-01 12:49:21 -0300

    Consulta simples com query methods

--- src/main/java/com/myprojeto/workshopMongoDB/resources/PostResource.java
+++ src/main/java/com/myprojeto/workshopMongoDB/resources/PostResource.java
@@ -1,13 +1,13 @@
 package com.myprojeto.workshopMongoDB.resources;
 
 import com.myprojeto.workshopMongoDB.domain.Post;
+import com.myprojeto.workshopMongoDB.resources.util.URL;
 import com.myprojeto.workshopMongoDB.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -22,4 +22,10 @@ public class PostResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decode(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
 }
--- src/main/java/com/myprojeto/workshopMongoDB/service/PostService.java
+++ src/main/java/com/myprojeto/workshopMongoDB/service/PostService.java
@@ -6,6 +6,7 @@ import com.myprojeto.workshopMongoDB.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class PostService {
     public Post findById(String id){
         Optional<Post> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+    }
+
+    public List<Post> findByTitle(String name){
+        return repository.findByTitleContainingIgnoreCase(name);
     }
 }
