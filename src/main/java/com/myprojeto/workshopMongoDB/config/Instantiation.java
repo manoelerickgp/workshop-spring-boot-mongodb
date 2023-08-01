794ab747d390c2fe3348ee1d48334bdb86292ec4
manoelerickgp <manoel.computacao@gmail.com>
2023-07-31 23:19:00 -0300

    Referenciando os posts do usuario

--- src/main/java/com/myprojeto/workshopMongoDB/config/Instantiation.java
+++ src/main/java/com/myprojeto/workshopMongoDB/config/Instantiation.java
@@ -29,19 +29,22 @@ public class Instantiation implements CommandLineRunner {
 
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-
+        //
         userRepository.deleteAll();
         postRepository.deleteAll();
-
+        //
         User maria  = new User(null, "Maria Brown", "maria@gmail.com");
         User alex  = new User(null, "Alex Grey", "alex@gmail.com");
         User bob  = new User(null, "Bob Green", "bob@gmail.com");
-
+        //
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
-
+        //
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
-
+        //
         postRepository.saveAll(Arrays.asList(post1, post2));
+        //
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
